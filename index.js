3139186ddfeb971e887d1a2078e1d56f884de4fa
Vikalp Mishra <mishravikalp896@gmail.com>
2023-09-21 07:28:50 +0530

    some work complete

--- index.js
+++ index.js
@@ -33,23 +33,37 @@ console.log(gameCard)
 // to shuffle the card
 
 const myNumber = (array) => {
-    for (let i = array.length -1; i > 0; i--) {
-        let j = Math.floor(Math.random() * (i+1))
+    for (let i = array.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1))
         console.log(i, j)
         let temp = array[i]
         array[i] = array[j]
         array[j] = temp
     }
     return array
-} 
+}
 
 const shuffleChild = myNumber(gameCard)
 
+// selecting the card
+// let clickCount = 0;
+// let firstCard = "";
+// let secondCard = "";
 
-for (let i = 0; i < gameCard.length; i++) {
+
+parentDiv.addEventListener('click', (event) => {
+    let current_Card = event.target;
+    // if (current_Card === "card-section") {
+    //     return false
+    // }
+    current_Card.classList.add('card-selected');
+});
+
+
+for (let i = 0; i < shuffleChild.length; i++) {
     const childDiv = document.createElement('div');
     childDiv.classList.add('card');
-    childDiv.dataset.name = gameCard[i].name;
-    childDiv.style.backgroundImage = `url('${gameCard[i].img}')`;     
+    childDiv.dataset.name = shuffleChild[i].name;
+    childDiv.style.backgroundImage = `url('${shuffleChild[i].img}')`;
     parentDiv.appendChild(childDiv);
 }
