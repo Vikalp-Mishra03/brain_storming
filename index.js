0d306cf9ec06954f4d824a2b1f02a3037b3bc0d1
Vikalp Mishra <mishravikalp896@gmail.com>
2023-09-21 07:06:44 +0530

    adding shuffle functionality

--- index.js
+++ index.js
@@ -26,10 +26,30 @@ let cardsArray = [
 ];
 const parentDiv = document.querySelector("#card-section");
 
-for (let i = 0; i < cardsArray.length; i++) {
+// duplicating the card
+const gameCard = cardsArray.concat(cardsArray)
+console.log(gameCard)
+
+// to shuffle the card
+
+const myNumber = (array) => {
+    for (let i = array.length -1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i+1))
+        console.log(i, j)
+        let temp = array[i]
+        array[i] = array[j]
+        array[j] = temp
+    }
+    return array
+} 
+
+const shuffleChild = myNumber(gameCard)
+
+
+for (let i = 0; i < gameCard.length; i++) {
     const childDiv = document.createElement('div');
     childDiv.classList.add('card');
-    childDiv.dataset.name = cardsArray[i].name;
-    childDiv.style.backgroundImage = `url('${cardsArray[i].img}')`;     
+    childDiv.dataset.name = gameCard[i].name;
+    childDiv.style.backgroundImage = `url('${gameCard[i].img}')`;     
     parentDiv.appendChild(childDiv);
 }
