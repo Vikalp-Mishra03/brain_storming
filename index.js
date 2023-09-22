193ae7f2b04e6ccce0c8d21983705e7104a4b906
Vikalp Mishra <mishravikalp896@gmail.com>
2023-09-22 06:41:09 +0530

    adding js file with complete logic

--- index.js
+++ index.js
@@ -24,18 +24,20 @@ let cardsArray = [
         'img': 'img/python.png',
     }
 ];
-const parentDiv = document.querySelector("#card-section");
 
-// duplicating the card
+
+const parentDiv = document.querySelector('#card-section');
+
+// step 2 to duplicate each card
 const gameCard = cardsArray.concat(cardsArray)
-console.log(gameCard)
 
-// to shuffle the card
 
-const myNumber = (array) => {
+// steps 3
+// Note that this method creates a new shuffled array instead of modifying the original one.
+const myNumbers = (array) => {
     for (let i = array.length - 1; i > 0; i--) {
         let j = Math.floor(Math.random() * (i + 1))
-        console.log(i, j)
+        // console.log(i, j)
         let temp = array[i]
         array[i] = array[j]
         array[j] = temp
@@ -43,27 +45,91 @@ const myNumber = (array) => {
     return array
 }
 
-const shuffleChild = myNumber(gameCard)
-
-// selecting the card
-// let clickCount = 0;
-// let firstCard = "";
-// let secondCard = "";
+const shuffledChild = myNumbers(gameCard)
+let clickCount = 0;
+let firstCard = "";
+let secondCard = "";
 
 
+// styling the match card
+const card_matches = () => {
+    let card_selected = document.querySelectorAll('.card_selected');
+
+    card_selected.forEach((curElem) => {
+        curElem.classList.add('card_match')
+    })
+}
+
+
+// step 7
+
+const resetGame = () => {
+    firstCard = "";
+    secondCard = "";
+    clickCount = 0;
+
+    let card_selected = document.querySelectorAll('.card_selected');
+
+    card_selected.forEach((curElem) => {
+        curElem.classList.remove('card_selected')
+    })
+
+}
+
+// step 4
 parentDiv.addEventListener('click', (event) => {
-    let current_Card = event.target;
-    // if (current_Card === "card-section") {
-    //     return false
-    // }
-    current_Card.classList.add('card-selected');
-});
+    let curCard = event.target;
+
+    if(curCard.id === "card-section"){return false }
+
+    clickCount ++;
+
+    if(clickCount < 3){
+
+        if(clickCount === 1){
+            firstCard = curCard.parentNode.dataset.name;
+            curCard.parentNode.classList.add('card_selected');
+        }else{
+            secondCard = curCard.parentNode.dataset.name;
+            curCard.parentNode.classList.add('card_selected');
+        }
+
+        if(firstCard !== "" && secondCard !== ""){
+            if(firstCard === secondCard){
+                // curCard.classList.add('card_match')
+                setTimeout(() => {
+                    card_matches()
+                    resetGame()
+                }, 1000)
+
+            }else{
+                setTimeout(() => {
+                    resetGame()
+                }, 1000)
+            }
+        }
+
+    }
+
+})
 
 
-for (let i = 0; i < shuffleChild.length; i++) {
-    const childDiv = document.createElement('div');
-    childDiv.classList.add('card');
-    childDiv.dataset.name = shuffleChild[i].name;
-    childDiv.style.backgroundImage = `url('${shuffleChild[i].img}')`;
-    parentDiv.appendChild(childDiv);
+// step 1 to add the card
+for(let i=0; i<shuffledChild.length; i++){
+
+    const childDiv = document.createElement('div')
+    childDiv.classList.add('card')
+    childDiv.dataset.name = shuffledChild[i].name;
+    const front_div = document.createElement('div');
+    front_div.classList.add('front-card')
+
+    const back_div = document.createElement('div');
+    back_div.classList.add('back-card')
+
+    back_div.style.backgroundImage = `url(${shuffledChild[i].img})`;
+
+    parentDiv.appendChild(childDiv)
+
+    childDiv.appendChild(front_div)
+    childDiv.appendChild(back_div)
 }
